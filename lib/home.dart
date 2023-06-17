15f445e50519ea1c5295cb0ba3742927205f7caa
Shuvam <gautamshuvam123@gmail.com>
2023-06-17 20:46:04 +0545

    background color and text color changed randomly after button clicked

--- lib/home.dart
+++ lib/home.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 ///function inside a class is simply called Method
@@ -10,16 +12,39 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   Color chnageColor = Colors.red;
-  bool oldColor = false;
+  Color bg = Colors.white;
+
+  //bool oldColor = false;
   void stateChange() {
-    if (oldColor == true) {
-      chnageColor = Colors.red;
-      oldColor = false;
-    } else {
-      chnageColor = Colors.green;
-      oldColor = true;
-    }
+    // if (oldColor == true) {
+    //   chnageColor = Colors.red;
+    //   oldColor = false;
+    // } else {
+    //   chnageColor = Colors.green;
+    //   oldColor = true;
+    // }
+    // setState(() {});
+
+//to change color of text via button clicked
+    var rng = Random();
+    chnageColor = Color.fromRGBO(
+      rng.nextInt(255),
+      rng.nextInt(255),
+      rng.nextInt(255),
+      rng.nextDouble(),
+    );
+    changeBackground(
+      rng.nextInt(255),
+      rng.nextInt(255),
+      rng.nextInt(255),
+      rng.nextDouble(),
+    );
+
     setState(() {});
+  }
+
+  void changeBackground(red, green, blue, op) {
+    bg = Color.fromRGBO(red, green, blue, op);
   }
 
   @override
@@ -27,6 +52,7 @@ class _HomePageState extends State<HomePage> {
     return MaterialApp(
       debugShowCheckedModeBanner: false, //remove the debug banner
       home: Scaffold(
+        backgroundColor: bg,
         appBar: AppBar(
           title: Text("Shuvam's UI"),
           centerTitle: true,
